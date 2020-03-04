130660c0e3a7a511f1f0caeeaf442995535ca5c0
Klayvan <klayvan972@gmail.com>
2020-03-04 16:13:25 +0100

    Rajout de la lecture du token dans un fichier .txt et d'une commande .ping pour vérifier si le bot fonctionne

--- klaybot.py
+++ klaybot.py
@@ -3,10 +3,25 @@ import json
 import os
 from discord.ext import commands
 import asyncio
-import botsetup
-from botsetup import token
 
-TOKEN = token()
+def read_token():
+    with open ("token.txt","r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+
+TOKEN = read_token()
 client = commands.Bot(command_prefix = ".")
 client.remove_command('help')
 os.chdir(r'D:\coding\Klaybot')
+
+@client.event
+async def on_ready():
+    #await client.change_presence(game=discord.Game(name="Etre un Dieu, Que faire ?",type=3))
+    print('Klaybot est en ligne.')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong! :ping_pong:')
+
+
+client.run(TOKEN)
